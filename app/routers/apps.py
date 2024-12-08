48f01a1feb8d7539a927881e7483edc5b6446690
Soton Ahmed <soton371@gmail.com>
2024-12-08 21:25:51 +0600

    app delete

--- app/routers/apps.py
+++ app/routers/apps.py
@@ -3,7 +3,7 @@ from fastapi import status, Depends, APIRouter
 from sqlalchemy.orm import Session
 from app.schemas import apps_schema
 from app.database import get_db
-from app.services.apps_service import create_app, get_all_apps
+from app.services.apps_service import create_app, get_all_apps, delete_app
 from app.custom_response import ResponseFailed, ResponseSuccess
 from app.logging_config import logger
 
@@ -12,34 +12,42 @@ router = APIRouter(
     tags=['Apps']
 )
 
+
 @router.post('/')
 async def create_apps(payload: apps_schema.AppsCreate, db: Session = Depends(get_db)):
     try:
         app = create_app(payload, db)
         if not app:
             return ResponseFailed(status_code=status.HTTP_208_ALREADY_REPORTED,
-                                message=f"App with package name {payload.package_name} already exists")
+                                  message=f"App with package name {payload.package_name} already exists")
         return ResponseSuccess(message="A new app has been successfully added")
     except Exception as error:
         logger(f"create_apps error: {error}")
         return ResponseFailed()
 
+
 @router.get('/')
-async def all_apps(db: Session = Depends(get_db),limit: int = None, skip: int = 0,
-                    search: Optional[str] = ''):
+async def all_apps(db: Session = Depends(get_db), limit: int = None, skip: int = 0,
+                   search: Optional[str] = ''):
     try:
-        apps = get_all_apps(db=db,limit=limit, skip=skip, search=search)
+        apps = get_all_apps(db=db, limit=limit, skip=skip, search=search)
         if not apps:
             return ResponseFailed(status_code=status.HTTP_404_NOT_FOUND, message="No apps yet")
-        apps_data = [apps_schema.AppsOut.model_validate(app).model_dump() for app in apps]
+        apps_data = [apps_schema.AppsOut.model_validate(
+            app).model_dump() for app in apps]
         return ResponseSuccess(data=apps_data)
     except Exception as error:
         logger(f"all_apps error: {error}")
         return ResponseFailed()
 
-@router.delete("/")
-async def delete_app(db: Session = Depends(get_db)):
+
+@router.delete("/{id}")
+async def delete_apps(id: int, db: Session = Depends(get_db)):
     try:
-        pass
+        is_delete_app = delete_app(id=id, db=db)
+        if is_delete_app:
+            return ResponseSuccess(message="An app was successfully deleted")
+        else:
+            return ResponseFailed(message=f"No app found with this id {id}")
     except Exception as error:
         return ResponseFailed()
--- app/services/apps_service.py
+++ app/services/apps_service.py
@@ -20,3 +20,12 @@ def create_app(payload: apps_schema.AppsCreate, db: Session):
 def get_all_apps(db: Session, limit: int = None, skip: int = 0,
                     search: Optional[str] = ''):
     return db.query(apps_model.Apps).filter(apps_model.Apps.package_name.contains(search)).limit(limit).offset(skip).all()
+
+def delete_app(id: int, db: Session):
+    exist_app = db.query(apps_model.Apps).filter(
+        apps_model.Apps.id == id)
+    if not exist_app.first():
+        return False
+    exist_app.delete(synchronize_session=False)
+    db.commit()
+    return True
